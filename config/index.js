0a0dba9ca58d156001d2e2a50803663f4281c9cf
caiwang1993 <505673793@qq.com>
2019-11-13 09:15:36 +0800

    vue-cli脚手架理解

--- config/index.js
+++ config/index.js
@@ -5,27 +5,42 @@
 const path = require('path')
 
 module.exports = {
+  //开发环境的基本配置
   dev: {
 
     // Paths
+    //编译输出的二级目录
     assetsSubDirectory: 'static',
+    //编译发布的跟目录，可配置成资源服务器域名或cdn域名
     assetsPublicPath: '/',
+    //需要使用proxyTable代理的 接口（可跨域）
     proxyTable: {},
 
     // Various Dev Server settings
+    //开发时的访问域名。可以通过环境变量自己设置
     host: 'localhost', // can be overwritten by process.env.HOST
+    //开发时的端口，可以通过环境变量port设定。如果端口被占用了会随机分配一个未被使用的端口号
     port: 8084, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    //是否自动打开浏览器
     autoOpenBrowser: false,
+    //在浏览器里是否展示错误蒙层
     errorOverlay: true,
+    //是否展示错误的通知
     notifyOnErrors: true,
+    //这个是webpack-dev-server的watchOption的一个选项，制定webpack检查文件的方式
+    //因为webpack使用文件系统去获取文件改变的通知。在有些情况下，这个可能不起作用。列如使用NFC的时候
+    //vagrant也会在这方面存在很多问题，在这些情况下，使用poll选项（以轮询的方式去检查文件是否改变），可以设置成true
+    //或者具体是数值，制定文件查询的具体时间周期
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
+    //是否使用Eslint检查代码
     useEslint: true,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
+    //如果设置为true，在浏览器中，eslint的错误和警告会以蒙层的方式展示
     showEslintErrorsInOverlay: false,
 
     /**
@@ -33,13 +48,15 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
+    //source maps 的格式
     devtool: 'cheap-module-eval-source-map',
 
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    //指定是否通过在文件名称后面添加一个查询字符串来创建source map的缓存
     cacheBusting: true,
-
+    //关闭css的source map
     cssSourceMap: true
   },
 
