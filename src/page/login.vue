31687d08f0029c9f81b7b3c7e1559460a910adec
caiwang1993 <505673793@qq.com>
2019-11-21 17:28:17 +0800

    登录页面

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,131 +1,206 @@
 <template>
 
-  	<div class="login_page fillcontain">
-	  	<transition name="form-fade" mode="in-out">
-	  		<section class="form_contianer" v-show="showLogin">
-		  		<div class="manage_tip">
-		  			<p>elm后台管理系统</p>
-		  		</div>
-          <el-form :model="loginForm" :rules="rules" ref="loginForm">
-            <el-form-item prop="username">
-              <el-input v-model="loginForm.username"  placeholder="用户名"></el-input>
-            </el-form-item>
-            <el-form-item prop="password">
-              <el-input v-model="loginForm.password" placeholder="密码"></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登录</el-button>
-            </el-form-item>
+  <div
+    class="login_page fillcontain">
+    <transition
+      name="form-fade"
+      mode="in-out">
+      <section
+        class="form_contianer"
+        v-show="showLogin">
+        <div
+          class="manage_tip">
+          <p>
+            elm后台管理系统</p>
+        </div>
+        <el-form
+          :model="loginForm"
+          :rules="rules"
+          ref="loginForm">
+          <el-form-item
+            prop="username">
+            <el-input
+              v-model="loginForm.username"
+              placeholder="用户名"></el-input>
+          </el-form-item>
+          <el-form-item
+            prop="password">
+            <el-input
+              v-model="loginForm.password"
+              placeholder="密码"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              type="primary"
+              @click="submitForm('loginForm')"
+              class="submit_btn">
+              登录
+            </el-button>
+          </el-form-item>
 
-          </el-form>
-				<p class="tip">温馨提示：</p>
-				<p class="tip">未登录过的新用户，自动注册</p>
-				<p class="tip">注册过的用户可凭账号密码登录</p>
-	  		</section>
-	  	</transition>
-  	</div>
+        </el-form>
+        <p
+          class="tip">
+          温馨提示：</p>
+        <p
+          class="tip">
+          未登录过的新用户，自动注册</p>
+        <p
+          class="tip">
+          注册过的用户可凭账号密码登录</p>
+      </section>
+    </transition>
+  </div>
 </template>
 
 <script>
-	import {login, getAdminInfo} from '@/api/getData'
+  import {
+    login,
+    getAdminInfo
+  } from '@/api/getData'
   //vuex是一个专门为vue.js应用程序开发是状态管理模式
-	import {mapActions, mapState} from 'vuex'
+  import {
+    mapActions,
+    mapState
+  } from 'vuex'
 
-	export default {
-	    //state,驱动应用的数据源
+  export default {
+    //state,驱动应用的数据源
     data(){
-	        return{
-            loginForm:{
-                username:'',
-                password:''
-            },
-            rules:{
-                username:[
-                  {required:true,message:'请输入用户名',trigger:'blur'}
-                ],
-              password:[
-                {required:true,message:'请输入密码',trigger:'blur'}
-              ]
+      return {
+        loginForm: {
+          username: '',
+          password: ''
+        },
+        rules: {
+          username: [
+            {
+              required: true,
+              message: '请输入用户名',
+              trigger: 'blur'
+            }
+          ],
+          password: [
+            {
+              required: true,
+              message: '请输入密码',
+              trigger: 'blur'
+            }
+          ]
 
-            },
-            showLogin:false
-          }
-		},
+        },
+        showLogin: false
+      }
+    },
     //mounted为html加载完成后执行。执行顺序：子组件-父组件
     mounted(){
-      this.showLogin=true;
-      if(!this.adminInfo.id){
-          this.getAdminData()
+      this.showLogin = true;
+      if (!this.adminInfo.id) {
+          console.log("this.getAdminData():"+this.getAdminData());
+        this.getAdminData()
       }
-      console.log("this指向："+this.showLogin)
+      console.log("adminInfo：" + this.adminInfo)
     },
     //created为html加载完成之前执行。执行顺序：父组件-子组件
     created(){
-        console.log(123)
+      console.log(123)
     },
     //计算属性 通过计算属性把数据进行运算
     //当一个组件需要获取多个状态的时候，将这些状态声明为计算属性会有些重复和冗余。
     //为了解决这个问题，我们可以使用mapState辅助函数帮助我们生成计算属性
-    computed:{
+    computed: {
       ...mapState(['adminInfo'])
     },
-    methods:{
-        //mapActions 的作用action函数绑定到methods里面，调用里面的方法时正常传参数
-      //...为es6中的对象扩展符
-      ...mapActions(['getAdminInfo']),
-
-      async submitForm(formName){
-        console.log("this.$refs[formName]:"+this.$refs[formName]),
-        this.$refs[formName].validata(async (valid) =>{
-          if(res.status == 1){
+    methods: {
+      ...mapActions(['getAdminData']),
+      async submitForm(formName) {
+        console.log("this.$refs[formName]:" + this.$refs[formName]);
+        this.$refs[formName].validate(async (valid) => {
+            console.log("valid:"+valid)
+          if (valid) {
+            const res =await login({user_name:this.loginForm.username,password:this.loginForm.password});
+            console.log("res:"+res);
+            if(res.status==1){
+                this.$message({
+                  type:'success',
+                  message:'登录成功'
+                });
+              this.$router.push('manage')
+            }else {
               this.$message({
-                type='success',
-
+                type:'error',
+                message:res.message
               })
+            }
+          } else {
+                this.$notify.error({
+                  title:'错误',
+                  message:'请输入正确的用户名密码',
+                  offset:100
+                })
           }
-        })
+        });
+      },
+    },
+    //使用watch的作用可以监控一个值的变换，并调用因为变化需要执行的方法。可以通过watch动态改变关联的状态
+    watch:{
+      adminInfo:function (newValue) {
+        if(newValue.id){
+            this.$message({
+              type:'success',
+              message:'检查到您之前登陆过，将自动登录'
+            });
+            //this.$router.push('manage')
+        }
       }
     }
-
-	}
+  }
 </script>
 
-<style lang="less" scoped>
-	@import '../style/mixin';
-	.login_page{
-		background-color: #324057;
-	}
-	.manage_tip{
-		position: absolute;
-		width: 100%;
-		top: -100px;
-		left: 0;
-		p{
-			font-size: 34px;
-			color: #333;
-		}
-	}
-	.form_contianer{
-		.wh(320px, 210px);
-		.ctp(320px, 210px);
-		padding: 25px;
-		border-radius: 5px;
-		text-align: center;
-		background-color: #fff;
-		.submit_btn{
-			width: 100%;
-			font-size: 16px;
-		}
-	}
-	.tip{
-		font-size: 12px;
-		color: red;
-	}
-	.form-fade-enter-active, .form-fade-leave-active {
-	  	transition: all 1s;
-	}
-	.form-fade-enter, .form-fade-leave-active {
-	  	transform: translate3d(0, -50px, 0);
-	  	opacity: 0;
-	}
+<style
+  lang="less"
+  scoped>
+  @import '../style/mixin';
+
+  .login_page {
+    background-color: #324057;
+  }
+
+  .manage_tip {
+    position: absolute;
+    width: 100%;
+    top: -100px;
+    left: 0;
+    p {
+      font-size: 34px;
+      color: #333;
+    }
+  }
+
+  .form_contianer {
+    .wh(320px, 210px);
+    .ctp(320px, 210px);
+    padding: 25px;
+    border-radius: 5px;
+    text-align: center;
+    background-color: #fff;
+    .submit_btn {
+      width: 100%;
+      font-size: 16px;
+    }
+  }
+
+  .tip {
+    font-size: 12px;
+    color: red;
+  }
+
+  .form-fade-enter-active, .form-fade-leave-active {
+    transition: all 1s;
+  }
+
+  .form-fade-enter, .form-fade-leave-active {
+    transform: translate3d(0, -50px, 0);
+    opacity: 0;
+  }
 </style>
