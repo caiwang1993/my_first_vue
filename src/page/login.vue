32ed4b69440b80158ee39e4b8ba1f7880db2f282
caiwang1993 <505673793@qq.com>
2019-11-21 14:42:27 +0800

    登录页面

--- src/page/login.vue
+++ src/page/login.vue
@@ -5,19 +5,19 @@
 	  		<section class="form_contianer" v-show="showLogin">
 		  		<div class="manage_tip">
 		  			<p>elm后台管理系统</p>
-
 		  		</div>
-		    	<el-form :model="loginForm" :rules="rules" ref="loginForm">
-					<el-form-item prop="username">
-						<el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
-					</el-form-item>
-					<el-form-item prop="password">
-						<el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
-					</el-form-item>
-					<el-form-item>
-				    	<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
-				  	</el-form-item>
-				</el-form>
+          <el-form :model="loginForm" :rules="rules" ref="loginForm">
+            <el-form-item prop="username">
+              <el-input v-model="loginForm.username"  placeholder="用户名"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input v-model="loginForm.password" placeholder="密码"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登录</el-button>
+            </el-form-item>
+
+          </el-form>
 				<p class="tip">温馨提示：</p>
 				<p class="tip">未登录过的新用户，自动注册</p>
 				<p class="tip">注册过的用户可凭账号密码登录</p>
@@ -31,76 +31,62 @@
   //vuex是一个专门为vue.js应用程序开发是状态管理模式
 	import {mapActions, mapState} from 'vuex'
 
-
 	export default {
 	    //state,驱动应用的数据源
-	    data(){
-			return {
-				loginForm: {
-					username: '',
-					password: '',
-				},
-				rules: {
-					username: [
-			            { required: true, message: '请输入用户名', trigger: 'blur' },
-			        ],
-					password: [
-						{ required: true, message: '请输入密码', trigger: 'blur' }
-					],
-				},
-				showLogin: false,
-			}
+    data(){
+	        return{
+            loginForm:{
+                username:'',
+                password:''
+            },
+            rules:{
+                username:[
+                  {required:true,message:'请输入用户名',trigger:'blur'}
+                ],
+              password:[
+                {required:true,message:'请输入密码',trigger:'blur'}
+              ]
+
+            },
+            showLogin:false
+          }
 		},
-		mounted(){
-			this.showLogin = true;
-			if (!this.adminInfo.id) {
-    			this.getAdminData()
-    		}
-		},
-		computed: {
-			...mapState(['adminInfo']),
-		},
-    //action，响应在view上的用户输入导致的状态变化
-		methods: {
-			...mapActions(['getAdminData']),
-			async submitForm(formName) {
-				this.$refs[formName].validate(async (valid) => {
-					if (valid) {
-						const res = await login({user_name: this.loginForm.username, password: this.loginForm.password})
-						if (res.status == 1) {
-							this.$message({
-		                        type: 'success',
-		                        message: '登录成功'
-		                    });
-							this.$router.push('manage')
-						}else{
-							this.$message({
-		                        type: 'error',
-		                        message: res.message
-		                    });
-						}
-					} else {
-						this.$notify.error({
-							title: '错误',
-							message: '请输入正确的用户名密码',
-							offset: 100
-						});
-						return false;
-					}
-				});
-			},
-		},
-		watch: {
-			adminInfo: function (newValue){
-				if (newValue.id) {
-					this.$message({
-                        type: 'success',
-                        message: '检测到您之前登录过，将自动登录'
-                    });
-					this.$router.push('manage')
-				}
-			}
-		}
+    //mounted为html加载完成后执行。执行顺序：子组件-父组件
+    mounted(){
+      this.showLogin=true;
+      if(!this.adminInfo.id){
+          this.getAdminData()
+      }
+      console.log("this指向："+this.showLogin)
+    },
+    //created为html加载完成之前执行。执行顺序：父组件-子组件
+    created(){
+        console.log(123)
+    },
+    //计算属性 通过计算属性把数据进行运算
+    //当一个组件需要获取多个状态的时候，将这些状态声明为计算属性会有些重复和冗余。
+    //为了解决这个问题，我们可以使用mapState辅助函数帮助我们生成计算属性
+    computed:{
+      ...mapState(['adminInfo'])
+    },
+    methods:{
+        //mapActions 的作用action函数绑定到methods里面，调用里面的方法时正常传参数
+      //...为es6中的对象扩展符
+      ...mapActions(['getAdminInfo']),
+
+      async submitForm(formName){
+        console.log("this.$refs[formName]:"+this.$refs[formName]),
+        this.$refs[formName].validata(async (valid) =>{
+          if(res.status == 1){
+              this.$message({
+                type='success',
+
+              })
+          }
+        })
+      }
+    }
+
 	}
 </script>
 
@@ -116,7 +102,7 @@
 		left: 0;
 		p{
 			font-size: 34px;
-			color: #fff;
+			color: #333;
 		}
 	}
 	.form_contianer{
